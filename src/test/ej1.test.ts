cf03c4731fae8f3ac526789b46566a717661d854
alansdorp <alexander.lansdorpgiron@nttdata.com>
2025-04-23 16:21:21 -0500

    feat: Test ej1

---
+++ src/test/ej1.test.ts
@@ -0,0 +1,48 @@
+import { TaskManager } from "../app/exercise/e1";
+describe("Test on ej1 component", () => {
+  test("Should call addTask and getTasks methods", async () => {
+    const instance = new TaskManager();
+    instance.addTask("test task");
+    const tasksList = instance.getTasks();
+    expect(tasksList).toEqual(["test task"]);
+  });
+  test("Should call markTaskAsCompleted method and return error", async () => {
+    try {
+      const instance = new TaskManager();
+      instance.markTaskAsCompleted(-1);
+    } catch (error) {
+      expect(error).toEqual(Error("Índice de tarea inválido"));
+    }
+  });
+  test("Should call markTaskAsCompleted method and return error", async () => {
+    try {
+      const instance = new TaskManager();
+      instance.markTaskAsCompleted(2);
+    } catch (error) {
+      expect(error).toEqual(Error("Índice de tarea inválido"));
+    }
+  });
+  test("Should addTask, call markTaskAsCompleted method and complete this task", async () => {
+    const instance = new TaskManager();
+    instance.addTask("test task");
+    instance.markTaskAsCompleted(0);
+  });
+  test("Should complete a task and clear completed tasks", async () => {
+    const instance = new TaskManager();
+    instance.addTask("test 1");
+    instance.addTask("test 2");
+    instance.addTask("test 3");
+    instance.addTask("test 4");
+    instance.markTaskAsCompleted(3);
+    instance.markTaskAsCompleted(2);
+    instance.clearCompletedTasks();
+    const tasksList = instance.getTasks();
+    expect(tasksList).toEqual(["test 1", "test 2"]);
+  });
+  test("Should call loadTasks", async () => {
+    const instance = new TaskManager();
+    instance.loadTasks(["task 1.1", "task 1.2", "task 1.3", "task 1.4"]);
+    const tasksList = instance.getTasks();
+    expect(tasksList).toEqual(["task 1.1", "task 1.2", "task 1.3", "task 1.4"]);
+  });
+});
