78430d603005db542f1b8f976df83171e0e8310d
alansdorp <alexander.lansdorpgiron@nttdata.com>
2025-04-23 15:27:01 -0500

    fix: Test config

--- itest.config.ts
+++ itest.config.ts
@@ -1,22 +1,15 @@
 import type { Config } from '@jest/types'
 
-const baseDir = '<rootDir>/src/app/server_app';
-const baseTestDir = '<rootDir>/src/test/server_app2';
+const baseDir = '<rootDir>/src/app';
+const baseTestDir = '<rootDir>/src/test';
 
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    verbose: true,
-    collectCoverage: true,
-    collectCoverageFrom: [
-        `${baseDir}/**/*.ts`
-    ],
-    testMatch:[
-        `${baseTestDir}/**/*test.ts`
-    ],
-    setupFiles: [
-        '<rootDir>/src/test/server_app3/utils/config.ts'
-    ]
-}
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [`${baseDir}/**/*.ts`],
+  testMatch: [`${baseTestDir}/**/*test.ts`],
+};
 
 export default config;
